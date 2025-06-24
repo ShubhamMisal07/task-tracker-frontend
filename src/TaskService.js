5b8144914c7d433e242c33c41ecb93c0a7d5dc32
ShubhamMisal07 <shubhamsunilmisal07@gmail.com>
2025-06-24 12:14:31 +0530

    Use environment variable for backend API base URL

--- src/TaskService.js
+++ src/TaskService.js
@@ -1,7 +1,7 @@
 // src/TaskService.js
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api/tasks';
+const BASE_URL = process.env.REACT_APP_API_BASE_URL + '/api/tasks';
 
 export const getAllTasks = () => axios.get(BASE_URL);
 export const createTask = (task) => axios.post(BASE_URL, task);
